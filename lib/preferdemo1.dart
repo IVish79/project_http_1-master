2b5c63ca63004f4a2007b8850d20f97e9a250cd1
vishal <ohobye3@gmail.com>
2022-09-20 09:50:41 +0530

    add

--- lib/preferdemo1.dart
+++ lib/preferdemo1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class preferdemo1 extends StatefulWidget {
   const preferdemo1({Key? key}) : super(key: key);
@@ -8,8 +9,19 @@ class preferdemo1 extends StatefulWidget {
 }
 
 class _preferdemo1State extends State<preferdemo1> {
-
   int a = 0;
+
+  SharedPreferences? prefs;
+
+  getpref() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  @override
+  void initState() {
+    getpref();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +65,27 @@ class _preferdemo1State extends State<preferdemo1> {
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 )),
+            SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await prefs!.setInt('MyValue1', a);
+                  await prefs!.setInt('MyValue2', a * 10);
+                },
+                child: Text("Store Value")),
+            SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  final int a = prefs!.getInt('MyValue1') ?? 0;
+                  final int b = prefs!.getInt('MyValue2') ?? 10;
+
+                  print(a);
+                  print(b);
+                },
+                child: Text("Get Value")),
           ],
         ),
       ),
